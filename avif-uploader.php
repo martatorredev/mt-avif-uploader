4e5bddec75b718f91afe23a721f2cc30e74684bd
martatorredev <hola@martatorre.dev>
2023-07-11 01:18:02 +0200

    In English

--- avif-uploader.php
+++ avif-uploader.php
@@ -1,62 +1,66 @@
 <?php
 /*
-	Plugin Name: AVIF Uploader
-	Description: Very simple plugin to create a works portfolio
-	Author: Marta Torre
-	Version: 1.0
-	License: GPLv3 or later
-	License URI: https://www.gnu.org/licenses/gpl-3.0.html
-	*/
+Plugin Name: MT AVIF Uploader
+Author: Marta Torre
+Author URI: https://martatorre.dev
+Text Domain: avif-uploader
+*/
 
-// Agrega el formulario al área de administración
+// Add the form to the admin area
 function avif_uploader_form() {
     ?>
     <div class="wrap">
-        <h1>AVIF Uploader</h1>
+        <h1><?php esc_html_e('AVIF Uploader', 'avif-uploader'); ?></h1>
         <form method="post" enctype="multipart/form-data">
             <input type="file" name="avif_file" accept=".avif">
-            <input type="submit" value="Subir AVIF">
+            <input type="submit" value="<?php esc_attr_e('Upload AVIF', 'avif-uploader'); ?>">
         </form>
     </div>
     <?php
 }
 
-// Procesa el formulario y sube el archivo AVIF
+// Process the form and upload the AVIF file
 function avif_upload_file() {
     if (isset($_FILES['avif_file'])) {
         $file = $_FILES['avif_file'];
 
-        // Verifica si se subió el archivo sin errores
+        // Check if the file was uploaded without errors
         if ($file['error'] === UPLOAD_ERR_OK) {
             $file_name = sanitize_file_name($file['name']);
             $file_tmp = $file['tmp_name'];
 
-            // Realiza las conversiones necesarias aquí
-            // Puedes usar bibliotecas o herramientas externas para convertir el archivo AVIF a otros formatos
+            // Perform necessary conversions here
+            // You can use libraries or external tools to convert the AVIF file to other formats
 
-            // Guarda el archivo convertido en una ubicación específica en el servidor
+            // Save the converted file to a specific location on the server
             $upload_dir = wp_upload_dir();
             $target_path = $upload_dir['path'] . '/' . $file_name;
 
             if (move_uploaded_file($file_tmp, $target_path)) {
-                // El archivo se subió correctamente
-                echo '¡El archivo AVIF se ha subido con éxito!';
+                // The AVIF file was uploaded successfully
+                echo esc_html__('The AVIF file has been uploaded successfully!', 'avif-uploader');
             } else {
-                // Ocurrió un error al mover el archivo
-                echo 'Ha ocurrido un error al subir el archivo AVIF.';
+                // An error occurred while moving the file
+                echo esc_html__('An error occurred while uploading the AVIF file.', 'avif-uploader');
             }
         } else {
-            // Ocurrió un error durante la subida del archivo
-            echo 'Ha ocurrido un error durante la subida del archivo AVIF.';
+            // An error occurred during the file upload
+            echo esc_html__('An error occurred during the AVIF file upload.', 'avif-uploader');
         }
     }
 }
 
-// Agrega los ganchos y acciones de WordPress
+// Add WordPress hooks and actions
 add_action('admin_menu', 'avif_uploader_menu');
 add_action('admin_init', 'avif_upload_file');
 
-// Agrega el menú en el panel de administración
+// Add the menu in the admin panel
 function avif_uploader_menu() {
-    add_menu_page('AVIF Uploader', 'AVIF Uploader', 'manage_options', 'avif-uploader', 'avif_uploader_form');
+    add_menu_page(
+        esc_html__('AVIF Uploader', 'avif-uploader'),
+        esc_html__('AVIF Uploader', 'avif-uploader'),
+        'manage_options',
+        'avif-uploader',
+        'avif_uploader_form'
+    );
 }
